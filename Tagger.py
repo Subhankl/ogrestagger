0428e02f863b28c5ed43a3321c3250303bbf0908
QocYeff <106670406+qocayefdi@users.noreply.github.com>
2022-08-13 17:55:11 +0400

    update Tagger.py

--- Tagger.py
+++ Tagger.py
@@ -231,7 +231,7 @@ async def mentionall(event):
     usrtxt = ""
     async for usr in client.iter_participants(event.chat_id):
       usrnum += 1
-      usrtxt += f"[{random.choice(mafia)}](tg://user?id={usr.id}) "
+      usrtxt += f"[{random.choice(fedler)}](tg://user?id={usr.id}) "
       if event.chat_id not in anlik_calisan:
         await event.respond("**Tag prosesini dayandırdınız ✅**")
         return
